485ba10fa4589d0d6edc06222a06812a840aa509
kaybee-mn <121321741+kaybee-mn@users.noreply.github.com>
2025-06-07 20:04:51 -0700

    Update Supabase client configuration to use VITE environment variables

--- src/config/supabaseAdmin.ts
+++ src/config/supabaseAdmin.ts
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const supabaseAdmin = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || '',
-  process.env.SUPABASE_SERVICE_ROLE_KEY || ''
+  process.env.VITE_SUPABASE_URL || '',
+  process.env.VITE_SUPABASE_SERVICE_KEY || ''
 );
 
 export { supabaseAdmin };
