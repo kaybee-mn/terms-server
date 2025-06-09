bef50c17f28e552c9db4f36f7cea73dc7d7ccf51
kaybee-mn <121321741+kaybee-mn@users.noreply.github.com>
2025-06-08 22:35:02 -0700

    Enhance title handling in simplifications route: add title insertion logic and improve error handling

--- src/routes/simplifications.ts
+++ src/routes/simplifications.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import verifyJWT from "../middleware/verifyJWT";
 import { supabaseAdmin } from "../config/supabaseAdmin";
+import { error } from "console";
 
 const router = express.Router();
 
@@ -11,9 +12,34 @@ router.post(
     const user_id = req.user?.id;
     const { title, content } = req.body;
     try {
+      // check if the title already exists in this user
+      const { data, error: selectionError } = await supabaseAdmin
+        .from("Documents")
+        .select("id")
+        .eq("user_id", user_id)
+        .eq("title", title);
+
+      if (selectionError) {
+        res.status(401).json({ error: "selection error" }).end();
+      }
+      let title_id;
+
+      if (data && data.length > 0) {
+        title_id = data[0].id;
+      } else {
+        // if the title doesn't already exist for this user, insert a new title
+        const { data: newDoc, error: insertError } = await supabaseAdmin
+          .from("Documents")
+          .insert({ title: title, user_id: user_id }).select("id");
+        if (insertError) {
+          res.status(401).json({ error: "insertion error" }).end();
+        }
+        title_id = newDoc ? newDoc[0].id : null;
+      }
+
       await supabaseAdmin
         .from("Simplifications")
-        .insert([{ user_id, title, content }]);
+        .insert([{ title_id, content }]);
     } catch (error) {
       console.error("Save error:", error);
       res
@@ -29,7 +55,6 @@ router.get(
   verifyJWT,
   async (req: express.Request, res: express.Response) => {
     const user_id = req.user?.id;
-    console.log(user_id)
     if (!user_id) {
       res.status(401).json({ error: "Unauthorized" }).end();
     }
@@ -38,8 +63,25 @@ router.get(
         .from("Documents")
         .select("title")
         .eq("user_id", user_id);
-      console.log(result);
-      res.json({ titles: result });
+      res.json(result);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "An error occurred while retrieving title list." });
+    }
+  }
+);
+
+router.get(
+  "/titles/:title_id",
+  verifyJWT,
+  async (req: express.Request, res: express.Response) => {
+    try {
+      const result = await supabaseAdmin
+        .from("Simplifications")
+        .select()
+        .eq("table_id", req.params.table_id);
+      res.json(result);
     } catch (error) {
       res
         .status(500)
